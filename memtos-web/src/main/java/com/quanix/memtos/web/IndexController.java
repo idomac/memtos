a39d71b560328577ba2f3265ea9eea6bc25db8a7
quanix <quanix@163.com>
2014-05-18 11:27:36 +0800

    提交相关Maven文件

---
+++ memtos-web/src/main/java/com/quanix/memtos/web/IndexController.java
@@ -0,0 +1,13 @@
+package com.quanix.memtos.web;
+
+/**
+ * created by quanix
+ *
+ * 首页控制器
+ */
+public class IndexController {
+
+    public String goIndex() {
+        return "index.jsp";
+    }
+}
